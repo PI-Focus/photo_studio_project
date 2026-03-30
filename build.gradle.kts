dd2fc4f7c72dd4dee2d82a34c516fa92a28c907c
Vadim <148389181+Vaad1m@users.noreply.github.com>
2026-03-30 15:51:45 +0300

    Initializing a Java Spring Boot project. (#14)
    - Configured gradle
    -  Created application's entry point
    - Created controller
    - Created samples
    - Created sample test
    - Made basic configuration for PMDs

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.5"
+	id("io.spring.dependency-management") version "1.1.7"
+    pmd
+}
+
+group = "pi.focus"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+pmd {
+    toolVersion = "7.23.0"
+    ruleSets = listOf()
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.named<Pmd>("pmdMain") {
+    ruleSetFiles = files("pmdMain.xml")
+}
+
+tasks.named<Pmd>("pmdTest") {
+    ruleSetFiles = files("pmdTest.xml")
+}
---
+++ src/test/java/pi/focus/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.focus.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
