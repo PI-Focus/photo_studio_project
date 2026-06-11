914ab4ba65eb230180e2383668a1a6020b7739ee
junealone-main <88303861+junealone-main@users.noreply.github.com>
2026-06-11 18:12:58 +0300

    Made models from model mocks

--- src/main/java/pi/focus/server/service/context/mocks/InfoContextMock.java
+++ src/main/java/pi/focus/server/service/context/mocks/InfoContextMock.java
@@ -7,35 +7,35 @@ import pi.focus.server.api.models.IAboutDataBlock;
 import pi.focus.server.api.models.IDataCard;
 import pi.focus.server.api.models.ITab;
 import pi.focus.server.api.models.ITextCard;
-import pi.focus.server.service.models.mocks.AboutDataBlockMock;
-import pi.focus.server.service.models.mocks.DataCardMock;
-import pi.focus.server.service.models.mocks.TabMock;
-import pi.focus.server.service.models.mocks.TextCardMock;
+import pi.focus.server.service.models.AboutDataBlockDto;
+import pi.focus.server.service.models.DataCardDto;
+import pi.focus.server.service.models.TabDto;
+import pi.focus.server.service.models.TextCardDto;
 
 public class InfoContextMock implements IInfoContext {
     private final List<ITextCard> rentRules;
     private final List<ITab<IDataCard>> imagedTabs;
-    private final AboutDataBlockMock aboutData;
+    private final AboutDataBlockDto aboutData;
 
     public InfoContextMock() {
         rentRules = List.of(
-            new TextCardMock("Первое правило", "Не упоминать о бойцовском клубе."),
-            new TextCardMock("Второе правило", "Нигде не упоминать о бойцовском клубе."),
-            new TextCardMock("Третье правило", "Если противник крикнул «стоп», выдохся или отключился — бой окончен."),
-            new TextCardMock("Четвёртое правило", "В бою участвуют лишь двое."),
-            new TextCardMock("Пятое правило", "В один вечер — только один поединок."),
-            new TextCardMock("Шестое правило", "Снимать обувь и рубашку."),
-            new TextCardMock("Седьмое правило", "Бой продолжается столько, сколько потребуется."),
-            new TextCardMock("Восьмое правило", "Тот, кто впервые пришел в клуб, примет бой.")
+            new TextCardDto("Первое правило", "Не упоминать о бойцовском клубе."),
+            new TextCardDto("Второе правило", "Нигде не упоминать о бойцовском клубе."),
+            new TextCardDto("Третье правило", "Если противник крикнул «стоп», выдохся или отключился — бой окончен."),
+            new TextCardDto("Четвёртое правило", "В бою участвуют лишь двое."),
+            new TextCardDto("Пятое правило", "В один вечер — только один поединок."),
+            new TextCardDto("Шестое правило", "Снимать обувь и рубашку."),
+            new TextCardDto("Седьмое правило", "Бой продолжается столько, сколько потребуется."),
+            new TextCardDto("Восьмое правило", "Тот, кто впервые пришел в клуб, примет бой.")
         );
 
         imagedTabs = List.of(
-            new TabMock<IDataCard>("ЗАЛЫ", new DataCardMock("Залы", "У нас вы можете выбрать зал йоу", MocksDefines.TEST_IMAGE_PATH, "/photorooms")),
-            new TabMock<IDataCard>("ОБОРУДОВАНИЕ", new DataCardMock("Оборудование", "У нас вы можете выбрать оборудование йоу", MocksDefines.TEST_IMAGE_PATH, "/equipment")),
-            new TabMock<IDataCard>("ФОТОГРАФЫ", new DataCardMock("Фотографы", "У нас вы можете выбрать фотографов йоу", MocksDefines.TEST_IMAGE_PATH, "/photographers"))
+            new TabDto<IDataCard>("ЗАЛЫ", new DataCardDto("Залы", "У нас вы можете выбрать зал йоу", MocksDefines.TEST_IMAGE_PATH, "/photorooms")),
+            new TabDto<IDataCard>("ОБОРУДОВАНИЕ", new DataCardDto("Оборудование", "У нас вы можете выбрать оборудование йоу", MocksDefines.TEST_IMAGE_PATH, "/equipment")),
+            new TabDto<IDataCard>("ФОТОГРАФЫ", new DataCardDto("Фотографы", "У нас вы можете выбрать фотографов йоу", MocksDefines.TEST_IMAGE_PATH, "/photographers"))
         );
 
-        aboutData = new AboutDataBlockMock(
+        aboutData = new AboutDataBlockDto(
             "/images/logo-bright.svg",
             "Фотостудия «Фокус» в Ярославле — это 5 отдельных пространств, чтобы каждая съёмка была в своей вселенной. " +
             "Минимализм, характерные интерьеры, бесшумный хромакей и профильный свет. Вам не придётся тащить с собой полкомнаты. " +
