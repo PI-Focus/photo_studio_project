2c4651d959d21d464ef291b5f5b676a1b54a9e51
junealone-main <88303861+junealone-main@users.noreply.github.com>
2026-06-14 13:55:25 +0300

    allowed favicon.ico

--- src/main/java/pi/focus/server/core/security/SecurityConfig.java
+++ src/main/java/pi/focus/server/core/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable) // TODO: delete this row
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/css/**", "/images/**", "/js/**", "/fonts/**", "/docs/**").permitAll()
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/fonts/**", "/docs/**", "/favicon.ico").permitAll()
                 .requestMatchers("/", "/photorooms", "/photorooms/{id}", "/equipment", "/photographers", "/login", "/login?*", "/registration").permitAll()
                 .requestMatchers("/profile").hasRole(UserRole.USER.name())
             ).formLogin(form -> form
