5d9db05fd79f6dd10b9b34350c7c150b21d4bc0d
junealone-main <88303861+junealone-main@users.noreply.github.com>
2026-06-15 23:23:25 +0300

    Feature/profile page (#222)
    * left pane design
    
    * deletion
    
    * WIP options page

--- src/main/java/pi/focus/server/controller/ProfileController.java
+++ src/main/java/pi/focus/server/controller/ProfileController.java
@@ -76,7 +76,7 @@ public class ProfileController {
         if (!uuid.equals(userDetails.userId())) {
             return redirectPrevious(session);
         } else {
-            return "pages/profile";
+            return "pages/profile-orders";
         }
     }
 
@@ -174,7 +174,7 @@ public class ProfileController {
         if (!uuid.equals(userDetails.userId())) {
             return "redirect:/";
         }
-        return "pages/login";
+        return "pages/profile-options";
     }
 
     /**
